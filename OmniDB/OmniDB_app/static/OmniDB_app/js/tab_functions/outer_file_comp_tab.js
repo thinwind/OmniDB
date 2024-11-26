2962f83228e1b03d5f0d4a06a22752ff4cef12d5
Shang Yehua <niceshang@outlook.com>
2024-11-26 20:50:40 +0800

    修改中

--- OmniDB/OmniDB_app/static/OmniDB_app/js/tab_functions/outer_file_comp_tab.js
+++ OmniDB/OmniDB_app/static/OmniDB_app/js/tab_functions/outer_file_comp_tab.js
@@ -1,55 +1,141 @@
 /**
  * 比较数据库结构文件
  */
-var v_createFileCompareTabFunction = function () {
-    let v_tab = v_connTabControl.createTab({
+var v_createFileCompareTabFunction = function (p_index, p_create_query_tab = true, p_name = false, p_tooltip_name = false) {
+    // Removing the last `add` tab
+    // v_connTabControl.removeLastTab();
+
+    var v_tab = v_connTabControl.createTab({
         p_icon: '<i class="fas fa-newspaper"></i>',
         p_name: 'Compare Databases',
-        p_close: false,
-        p_selectable: false,
-        p_clickFunction: function (e) {
-            showAlert('Compare Databases based on files');
+        p_selectFunction: function () {
+            document.title = 'Welcome to OmniDB'
+            $('[data-toggle="tooltip"]').tooltip({ animation: true });// Loads or Updates all tooltips
         },
-        p_omnidb_tooltip_name: '<h5 class="my-1">Compare Databases based on files</h5>'
+        p_close: false,// Replacing default close icon with contextMenu.
+        p_closeFunction: function (e, p_tab) {
+            var v_this_tab = p_tab;
+            beforeCloseTab(e,
+                function () {
+                    v_this_tab.removeTab();
+                });
+        },
+        p_rightClickFunction: function (e) {
+            var v_option_list = [
+                {
+                    text: '<p class=\"mb-0 text-danger\">Close Welcome Tab</p>',
+                    action: function () {
+                        if (v_tab.closeFunction != null) {
+                            v_tab.closeFunction(e, v_tab);
+                        }
+                    }
+                }
+            ];
+            customMenu(
+                {
+                    x: e.clientX + 5,
+                    y: e.clientY + 5
+                },
+                v_option_list,
+                null);
+        },
+        p_omnidb_tooltip_name: '<h5 class="my-1">Welcome to OmniDB</h5>'
     });
 
     v_connTabControl.selectTab(v_tab);
 
+    var v_width = Math.ceil((300 / window.innerWidth) * 100);
+    var v_complement_width = 100 - v_width;
+
+
+    // Title html string
+    let v_html_title =
+        '<h1 class="mb-4" style="padding-left: 100px; position: relative;">' +
+        '<span class="omnidb__welcome__intro-text">File Compare Page<span style="color:#4a6cbb;">OmniDB!</span></span>' +
+        '</h1>';
+    // Intro html string
+    let v_html_intro =
+        '<div class="card p-3 omnidb__welcome__intro-card">' +
+
+        '<p class="text-center"><span class="badge badge-danger" style="vertical-align: middle;">disclaimer</span> OmniDB is a powerful tool, and with great power...<br/>Please <strong><span class="text-danger">learn how to use it on a testing environment, NOT on production</span></strong>!</p>' +
+
+
+        '<button type="button" class="btn btn-lg omnidb__theme__btn--primary w-auto mx-auto my-4" onclick="startTutorial(' + "'getting_started'" + ');">' +
+        '<i class="fas fa-list mr-2"></i>' +
+        'Getting started' +
+        '</button>' +
+
+
+        '<div class="alert-info p-2 rounded mt-4" style="display: grid; grid-template: \'icon text\';">' +
+        '<i class="fas fa-exclamation-triangle p-4" style="grid-area: icon;"></i>' +
+        '<div style="grid-area: text;">' +
+        `
+          Our focus is to provide a very flexible, secure and work-effective environment for multiple DBMS.<br>
+          With that in mind, you should <strong>be aware the many actions on the UI can lead to a direct interaction with the database</strong> that you are connected with.</br>
+          ` +
+        '</div>' +
+        '</div>' +
+        '</div>';
+
+    let v_html_useful_links =
+        '<div class="alert alert-success p-3 omnidb__welcome__useful-card">' +
+        '<h2 class="text-center mb-4">Useful stuff</h2>' +
+        '<ul>' +
+        '<li class="mb-2"><a class="btn btn-success text-white" target="_blank" href="https://omnidb.org"><i class="fas fa-user"></i> <span>OmniDB website</span></a></li>' +
+        '<li class="mb-2"><a class="btn btn-success text-white" target="_blank" href="https://github.com/OmniDB/OmniDB"><i class="fab fa-github"></i> <span>Github repo</span></a></li>' +
+        '<li><a class="btn btn-success text-white" target="_blank" href="https://omnidb.readthedocs.io/"><i class="fas fa-list"></i> <span>Read the docs</span></a></li>' +
+        '</ul>' +
+        '</div>';
+    // Template html string
     var v_html =
-        '<div style="position: relative;">' +
-      '<div style="display: grid; grid-template-areas: \'left right\'; grid-template-columns: auto minmax(0, 1fr);">' +
-        '<div id="' + v_tab.id + '_div_left" class="omnidb__workspace__div-left col" style="max-width: 300px; width: 300px;">' +
-          "<div class='row'>" +
+        '<div class="container" style="position: relative;">' +
+        '<div class="row">' +
+        '<div class="col-12">' +
 
-            // "<div onmousedown='resizeHorizontal(event)' style='width: 10px; height: 100%; cursor: ew-resize; position: absolute; top: 0px; right: 0px;'><div class='resize_line_vertical' style='width: 5px; height: 100%; border-right: 1px dashed #acc4e8;'></div><div style='width:5px;'></div></div>" +
+        // Welcome main block
+        '<div id="' + v_tab.id + '_welcome" class="omnidb__welcome" style="height: 100vh;display: flex;align-items: center;font-size: 1.2rem;justify-content: center;">' +
+        '<div>' +
+        // Title
+        v_html_title +
+        // Welcome grid
+        '<div style="display: grid; grid-template: \'intro getting_started links\'; grid-gap: 64px;">' +
+        // Intro area
+        '<div style="grid-area: intro;">' +
+        v_html_intro +
+        '</div>' +
+        // Getting started area
+        // '<div style="grid-area: getting_started">' +
+        //   v_html_getting_started +
+        // '</div>' +
+        // Links area
+        '<div style="grid-area: links;">' +
+        v_html_useful_links +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
 
-            '<div class="omnidb__workspace__content-left">' +
-              '<div id="' + v_tab.id + '_details" class="omnidb__workspace__connection-details"></div>' +
-              '<div id="' + v_tab.id + '_tree" style="overflow: auto; flex-grow: 1; transition: scroll 0.3s;"></div>' +
-              '<div id="' + v_tab.id + '_left_resize_line_horizontal" class="omnidb__resize-line__container" onmousedown="resizeTreeVertical(event)" style="width: 100%; height: 5px; cursor: ns-resize;"><div class="resize_line_horizontal" style="height: 0px; border-bottom: 1px dashed #acc4e8;"></div><div style="height:5px;"></div></div>' +
-              '<div id="tree_tabs_parent_' + v_tab.id + '" class="omnidb__tree-tabs omnidb__tree-tabs--not-in-view" style="position: relative;flex-shrink: 0;flex-basis: 280px;">' +
-                '<div id="' + v_tab.id + '_loading" class="div_loading" style="z-index: 1000;">' +
-                  '<div class="div_loading_cover"></div>' +
-                  '<div class="div_loading_content">' +
-                  '  <div class="spinner-border text-primary" style="width: 4rem; height: 4rem;" role="status">' +
-                  '    <span class="sr-only ">Loading...</span>' +
-                  '  </div>' +
-                  '</div>' +
-                '</div>' +
-                '<button type="button" onclick="toggleTreeTabsContainer(' + "'tree_tabs_parent_" + v_tab.id + "','" + v_tab.id + "_left_resize_line_horizontal'" + ')" class="btn omnidb__theme__btn--secondary omnidb__tree-tabs__toggler"><i class="fas fa-arrows-alt-v"></i></button>' +
-                '<div id="tree_tabs_' + v_tab.id + '" class="omnidb__tree-tabs__container" style="position: relative;"></div>' +
-              '</div>' +
-            '</div>' +
-          '</div>' +
-          '<div class="resize_line_vertical omnidb__resize-line__container" onmousedown="resizeConnectionHorizontal(event)" style="position:absolute;height: 100%;width: 10px;cursor: ew-resize;border-right: 1px dashed #acc4e8;top: 0px;right: 0px;"></div>' +
-        '</div>' +//.div_left
-        '<div id="' + v_tab.id + '_div_right" class="omnidb__workspace__div-right col" style="position: relative;">' +
-          // "<div class='row'>" +
-            '<button type="button" class="py-4 px-0 btn omnidb__theme__btn--secondary omnidb__tree__toggler" onclick="toggleTreeContainer()"><i class="fas fa-arrows-alt-h"></i></button>' +
-            '<div id="' + v_tab.id + '_tabs" class="w-100"></div>' +
-          // "</div>" +
-        '</div>' +//.div_right
-      '</div>' +//.row
+        '</div>' +//.row
 
-    '</div>';
-}
+        '</div>';
+
+    v_tab.elementDiv.innerHTML = v_html;
+
+    var v_tag = {
+        tab_id: v_tab.id,
+        divWelcome: document.getElementById(v_tab.id + '_welcome'),
+        selectedDatabaseIndex: 0,
+        connTabControl: v_connTabControl,
+        mode: 'welcome'
+    };
+
+    v_tab.tag = v_tag;
+
+    // Creating `Add` tab in the outer tab list
+    // v_connTabControl.createAddTab();
+
+    $('[data-toggle="tooltip"]').tooltip({ animation: true });// Loads or Updates all tooltips
+
+    endLoading();
+
+};
