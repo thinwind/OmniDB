1829a69eab58cdf9010746941a25418efc252891
Shang Yehua <niceshang@outlook.com>
2024-11-30 09:48:04 +0800

    删除部署相关内容

--- OmniDB/deploy.old/app_centos7/clone.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-
-rm -rf ~/OmniDB
-git clone --depth 1 --branch dev https://github.com/OmniDB/OmniDB ~/OmniDB
--- deploy/linux/build_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd tarbuild
-docker build -t omnidb/tarbuild .
-cd ..
-
-cd pkgbuild
-docker build -t omnidb/pkgbuild .
-cd ..
