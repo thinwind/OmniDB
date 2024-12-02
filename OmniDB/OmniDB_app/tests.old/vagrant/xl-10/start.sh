21a4e6e3f97680e15fbdcc38b9a25af03d6fd8f5
Shang Yehua <niceshang@outlook.com>
2024-12-02 14:52:41 +0800

    删除test.old

--- OmniDB/OmniDB_app/tests.old/vagrant/xl-10/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $1 = "gtm" ]
-then
-  su - postgres -c "/usr/local/pgsql/bin/gtm -D /usr/local/pgsql/data >logfile 2>&1 &"
-else
-  su - postgres -c "/usr/local/pgsql/bin/postgres --$1 -D /usr/local/pgsql/data >logfile 2>&1 &"
-fi
